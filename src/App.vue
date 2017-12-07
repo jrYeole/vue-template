e5399bf22ce82fe888657bfcba56a6e22fdc1bf6
Prajkt Yeole <prajkty@mkcl.org>
2017-12-07 17:24:25 +0530

    i18n WIP

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-   this is default page
+   <!-- {{ $t('heading') }} -->
+   this is default heading
+   <router-link to="home">Home</router-link>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// import VueI18n from 'vue-i18n'
+
+// Vue.use(VueI18n)
+// Vue.use(vueI18n.plugin, i18n)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  // i18n,
   template: '<App/>',
   components: { App }
 })
